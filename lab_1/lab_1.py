4c75e84e6bf2791ed0e3395c3666a3992ff0d08a
Mary <marybelka04@gmail.com>
2024-04-13 23:44:58 +0400

    docstring and PEP-8

--- lab_1/lab_1.py
+++ lab_1/lab_1.py
@@ -5,6 +5,14 @@ import const
 
 
 def read_txt(file_path: str) -> str:
+    """Function reads the file at the specified path and saves it to a variable
+
+    Args:
+        file_path (str): Path to a file
+
+    Returns:
+        str: a variable contains data from the file
+    """
     try:
         with open(file_path, "r", encoding="UTF-8") as file:
             return file.read()
@@ -13,6 +21,12 @@ def read_txt(file_path: str) -> str:
 
 
 def write_txt(data: str, file_path: str) -> None:
+    """Function writes data into the file at the specified path
+
+    Args:
+        data (str): the data which needs to be into the file
+        file_path (str): path to the file where data was saved
+    """
     try:
         with open(file_path, 'w', encoding='utf-8') as file:
             file.write(str(data))
@@ -21,6 +35,14 @@ def write_txt(data: str, file_path: str) -> None:
 
 
 def count_character_frequency(text: str) -> dict:
+    """Function calculates the index of the frequency of characters found in a given text
+
+    Args:
+        text (str): The text analysed for the frequency index
+
+    Returns:
+        dict: key - a symbol, value - the index of the frequency
+    """
     character_frequency = {}
     text = text.lower()
     for char in text:
@@ -37,6 +59,11 @@ def count_character_frequency(text: str) -> dict:
 
 
 def read_json(file_path: str) -> dict[str:str]:
+    """Function reads the json-file at the specified path and saves it to a variable
+
+    Returns:
+        _type_: data from json-file
+    """
     try:
         with open(file_path, 'r', encoding='utf-8') as file:
             return json.load(file)
@@ -45,6 +72,15 @@ def read_json(file_path: str) -> dict[str:str]:
 
 
 def encrypt(text: str, key: dict[str, str]) -> str:
+    """Function encrypts the text using a key
+
+    Args:
+        text (str): text for cyphering
+        key (dict[str, str]): key for cyphering
+
+    Returns:
+        str: encrypted text
+    """
     result = ""
     for lit in text:
         if lit in key.keys():
