0adcc183098e65a7548d17a5a5c82388cce6eac7
Lisyonok04 <125237397+Lisyonok04@users.noreply.github.com>
2024-04-13 18:27:42 +0400

    Update лаба1.py

--- lab_1/лаба1.py
+++ lab_1/лаба1.py
@@ -8,8 +8,16 @@ def read_txt(file_path: str) -> str:
     with open(file_path, "r", encoding="UTF-8") as file:
         return file.read()
 
+def write_txt(data: str, file_path: str) -> None:
+    """
+    function, which writes text in .txt file
 
-def count_character_frequency(text) -> dict:
+    It takes as arguments data, which it should write into, and path to the file
+    """
+    with open(file_path, 'w', encoding='utf-8') as file:
+        file.write(str(data))
+
+def count_character_frequency(text: str) -> dict:
     """
     function, which counts the frequency of every symbol in the text file
 
@@ -29,10 +37,7 @@ def count_character_frequency(text) -> dict:
     
     return dict(character_index)
 
-text = "И7У24>2 >МР4ДД >М2ЕПЧЙМД48 О4ЙАИЛМrЕt48ДЕ2ЧММИЙtЕХ4МШ4Ф1МЙ>ХИЙМУ1УМРОЕУ<Д >МР4ДД >МУ1УМЕЪУШtЕО4ДД ФМИУПД41МИМЙЕ<ХУМ8t>ДУЛМЙ>ЕtУУМrt>РrЕ14П4>ЙИЛМ<ЙЕМР4ДД >МrtЕУ8ОЕРЛЙИЛМУИЙЕ<ДУХЕ2МУМrt>РЕИЙ4О1ЛКЙИЛМХЕ2rt>ИИЕtЧМОМ ОУР>МИУ2ОЕ14МД4РМД>ХЕЙЕt 2М41Ш4ОУЙЕ2Мt4ДАЫ>МО>ИАМrtЕЪ>ИИМИ74ЙУЛМД48 О41УМХЕРУtЕО4ДУ>2МУИЙЕ<ДУХ4МrЕИХЕ1АХЧМЕДЕМrtУ8О4ДЕМЧР41УЙ АМУ85 ЙЕ<ДЕИЙАМОМР4ДД ЩМД4МЕИДЕО>МУЩМrt>РИХ48Ч>2ЕИЙУМrЧЙ>2МУДЕПЕМrt>РИЙ4О1>ДУЛ МР4ДД ЩММЙЕМ>ИЙАМУЩМХЕРУtЕО4ДУЛМ84МРО4МrЕИ1>РДУЩМР>ИЛЙУ1>ЙУ ЛМХ4tЙУД4МД>ИХЕ1АХЕМУ82>ДУ14ИАМr>tОЕФМ14ИЙЕ<ХЕФМИЙ414МУР> ЛМt48Р>1УЙАМrtЕЪ>ИИМИ74ЙУЛМД4МРО4МО84У2ЕИОЛ84ДД ЩМrtЕЪ>ИИ4МХЕРУtЕО4ДУ>МД>rЕИt>РИЙО>ДДЕМОЕИrtЕУ8ОЕРЛЬ>>МИ74Й ФМrЕЙЕХМИУ2ОЕ1ЕОМУМ2ЕР>1УtЕО4ДУ>Мrt>РЕИЙ4О1ЛКЬ>>МОИКМД>Е5ЩЕРУ2ЧК МР1ЛМХЕРУtЕО4ДУЛМУДШЕt24ЪУК"
-character_index = count_character_frequency(text)
+coded = read_txt("D:\оиб\лаба 1\original text.txt")
+character_index = count_character_frequency(coded)
 
-for char, index in character_index.items():
-    print(f"{char}, {index}")
-
-text_de = "сжимаемые данные могут называться поразному строка файл текст или двоичные данные или оцифрованный сигнал с точки зрения теории предполагается что данные производятся источником и предоставляются компрессору в виде символа над некоторым алфавитом раньше весь процесс сжатия называли кодированием источника поскольку оно призвано удалить избыточность в данных на основе их предсказуемости путем иного представления данных то есть их кодирования за два последних десятилетия картина несколько изменилась первой ласточкой стала идея разделить процесс сжатия на два взаимосвязанных процесса кодирование непосредственно воспроизводяЬее сжатый поток символов и моделирование предоставляющее всю необходимую для кодирования информацию"
+write_txt(character_index, "D:/оиб/лаба 1/freq.txt")
