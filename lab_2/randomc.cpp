a21c6f8eb08201fa62631845e665d78b3b69bba0
Mary <marybelka04@gmail.com>
2024-05-08 22:06:17 +0400

    random 2.0

--- lab_2/randomc.cpp
+++ lab_2/randomc.cpp
@@ -1,32 +1,31 @@
 #include <iostream>
 #include <random>
-
+using namespace std;
 /*
- * The function for generating random 
- * numbers in a given range
- * @param a, b The boundaries of the range for generating a random number
- * @return x The generated random number from the random function
+ * The function for generating random numbers in a given range
+ * @param a, b The ranges for generating a random number
+ * @return A random number from the random function
  */
 int random(int a, int b) {
-    std::random_device random_device;
-    std::mt19937 generator(random_device());
-    std::uniform_int_distribution<> segment(a, b);
+    random_device random_device;
+    mt19937 generator(random_device());
+    uniform_int_distribution<> segment(a, b);
     int x = segment(generator);
     return x;
 }
 
 /*
- * The function for generating a random s equence of
+ * The function for generating a random sequence of
  * bits of a given length and displaying it on the screen
  * @param n The number of bits in the sequence 
  */
 void RandomGenerator(int n) {
     for(int i = 0; i < n; i++) 
-        std::cout << random(0, 1);
+        cout << random(0, 1);
 }
 
 int main() {
-    const int NUMBER = 128;
-    RandomGenerator(NUMBER);
+    const int BTS = 128;
+    RandomGenerator(BTS);
     return 0; 
 }
