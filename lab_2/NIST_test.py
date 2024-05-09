c60becd6d1a0ae063f0401d047b609877b23123b
Mary <marybelka04@gmail.com>
2024-05-09 18:58:06 +0400

    NIST third test

--- lab_2/NIST_test.py
+++ lab_2/NIST_test.py
@@ -4,7 +4,7 @@ import mpmath
 
 from typing import Dict
 
-from const import PATH
+from const import PATH, PI
 
 def get_json(name: str) -> Dict[str, str]:
     """Function reads the json-file at the specified path 
@@ -58,13 +58,75 @@ def identical(name: str) -> float:
     return p
 
 
+def max_sequence(name: str) -> int:
+    """
+    The function searches for the maximum length of a
+    sequence
+
+    Args:
+             name(str): path .json file
+    Returns:
+             maxline(int): longest sequence
+    """
+    maxline = 0
+    count = 0
+    for i in name:
+        if i == "1":
+            count += 1
+            maxline = max(maxline, count)
+        else:
+            #maxline = max(maxline, count)
+            count = 0
+    return maxline
+
+
+def longest_sequence(name: str) -> float:
+    """The function analyzes for the longest sequence in block
+
+    Args:
+            name(str): random sequence
+
+    Returns:
+            p(float): P-value
+    """
+    max_len = {}
+    length = len(name)
+    v = {1: 0, 2: 0, 3: 0, 4: 0}
+    for step in range(0, length, 8):
+        block = name[step: step + 8]
+        block_max_len = max_sequence(block)
+        if block_max_len in max_len:
+            max_len[block_max_len] += 1
+        else:
+            max_len[block_max_len] = 1
+    for i in max_len:
+        if i <= 1:
+            v[1] += max_len[i]
+        elif i == 2:
+            v[2] += max_len[i]
+        elif i == 3:
+            v[3] += max_len[i]
+        else:
+            v[4] += max_len[i]
+    xi_squared = 0
+    for i in range(4):
+            xi_squared += ((v[i + 1] - 16 * PI[i + 1]) ** 2) / (16 * PI[i + 1])
+    p = mpmath.gammainc(3 / 2, xi_squared / 2)
+    return p
+
+
 def main() -> None:
     c = get_json(PATH)["C++"]
     java = get_json(PATH)["java"]
-    #print(frequency(c))
-    #print(frequency(java))
+    print("results for C++: ")
+    print(frequency(c))
     print(identical(c))
+    print(longest_sequence(c))
+    print("results for Java: ")
+    print(frequency(java))
     print(identical(java))
+    print(longest_sequence(java))
+    
 
 
 if __name__ == "__main__":
